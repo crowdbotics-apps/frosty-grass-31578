220be7525ab312bb164e236c8fa92cb21de17deb
Crowdbotics <team@crowdbotics.com>
2021-10-20 01:22:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/frosty_grass_31578/MainActivity.java
@@ -0,0 +1,15 @@
+package com.frosty_grass_31578;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "frosty_grass_31578";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "frosty_grass_31578",
+  url: "https://frosty_grass_31578.botics.co",
+  api: "https://frosty_grass_31578.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
